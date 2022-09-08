e39211aea3da636786b7bf9a562f6f27bc89f4b4
oluwole2044 <olukay08@gmail.com>
2022-09-08 12:22:07 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
 int main(void)
 {
+	/* print pout the string*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
