8ecf6ced4085754598b4c2761fc62ac18d737ce4
oluwole2044 <olukay08@gmail.com>
2022-09-08 14:46:05 -0700

    y

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	char h;
 	long int j;
 	long long int n;
-	printf("size of :%zu bytes\n",sizeof(p));
-	printf("size of float :%zu bytes \n",sizeof(t));
-	printf("size of char :%zu bytes \n",sizeof(h));
-	printf("size of long int :%zuu bytess \n", sizeof(n));
-	printf("size ofdouble :%zu bytes \n",sizeof(j));
+	printf("size of a char :%zu byte(s)\n", sizeof(h));
+	printf("size of an int :%zu byte(s) \n", sizeof(p));
+	printf("size of a long int :%zu byte(s) \n", sizeof(j));
+	printf("size of a long long int :%zu byte(s) \n", sizeof(n));
+	printf("size of a float :%zu byte(s) \n", sizeof(t));
 	return (0);
 }
