50d2bf2066ca285fea5a9fc60ee176214c599fea
oluwole2044 <olukay08@gmail.com>
2022-09-10 00:44:09 -0700

    rt

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 0 ; g < 10 ; g++)
+	{
+		putchar(g + '0');
+	}
+	putchar('\n');
+	return (0);
+}
