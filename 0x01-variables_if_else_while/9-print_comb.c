0d970c6875d08ad1179c476e01e8a25bb64b699e
oluwole2044 <olukay08@gmail.com>
2022-09-10 03:55:15 -0700

    hello world

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 0 ; g < 10 ; g++)
+	{
+		putchar(g + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
