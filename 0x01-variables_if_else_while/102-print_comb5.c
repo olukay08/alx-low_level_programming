0d5f8d8b2061acd3b7d14bbd3e305a6cf1ee80a2
oluwole2044 <olukay08@gmail.com>
2022-09-11 03:58:24 -0700

    hello world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,14 @@ int main(void)
 
 		for (k = 1; k < 100 ; k++)
 		{
-			if (1 < k ){
+			if (i < k )
+			{
 
-		putchar(i/10 + '0');
-		putchar(i%10 + '0');
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
 		putchar(' ');
-		putchar(k/10 + '0');
-		putchar(k%10 + '0');
+		putchar(k / 10 + '0');
+		putchar(k % 10 + '0');
 		if (i != 98 ||  k != 99)
 			{
 				putchar(',');
