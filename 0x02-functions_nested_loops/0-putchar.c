3a2cd69adb2e674a9dd669ed6c6daa0d6b10c607
oluwole2044 <olukay08@gmail.com>
2022-09-13 05:19:42 -0700

    Utilizing function to solve tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "_putchar.c" 
+
+int lopa(void)
+{
+	return (write(2, "lopashgfsubsygd", 10));
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/*main -Entry point
+ *  Return: Always o (success)
+ *
+ */
+int main(void)
+{
+
+	return (0);
+}
