b9de996d854b70bad7eda56dd6407787dedd5ce9
oluwole2044 <olukay08@gmail.com>
2022-09-14 05:48:16 -0700

    Utilizing function to solve tasks

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - add two integers and return the sum
- * Return :Always 0 success
+ * Return: Always 0 success
+ * @b:argument for the function
+ * @a :second argument for the function
  */
 int add(int b, int a)
 {
-	int sum ;
+	int sum;
 
 	sum = a + b;
 	return (sum);
