6f791f25dec31123a9036dbc748d8298f394e259
oluwole2044 <olukay08@gmail.com>
2022-09-14 01:55:45 -0700

    Utilizing function to solve tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauver - print every minutes in of the day of jack-bauver,starts
+ * Return :Always 0
+ */
+void jack_bauer(void)
+{
+	int h= 0;
+
+	int m= 0;
+
+	int hm;
+
+	int mr;
+
+	while (h < 24)
+	{
+		while (m < 59)
+		{
+			hm = h % 10;
+			mr =m %10;
+			_putchar(h / 10 + '0');
+			_putchar(hm + '0');
+			_putchar(':');
+			_putchar(m /10 + '0');
+			_putchar(m % 10 + '0');
+			m++;
+			_putchar('\n');
+		}
+		h++;
+		m = 0;
+	}
+}
