099c5d3f57e28fc1d913c0d254bd852b201333c5
oluwole2044 <olukay08@gmail.com>
2022-09-14 05:22:59 -0700

    Utilizing function to solve tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers and return the sum
+ * Return :Always 0 success
+ */
+int add(int b, int a)
+{
+	int sum ;
+
+	sum = a + b;
+	_putchar(sum + '0');
+	return (sum);
+return (0);
+}
