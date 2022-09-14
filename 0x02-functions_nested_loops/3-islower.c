4592b9cbc6d263fdfa5183bfdab124e5c6a326c0
oluwole2044 <olukay08@gmail.com>
2022-09-13 20:07:59 -0700

    Utilizing function to solve tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - function to check for lower character
+ * @c: is th int that will use for the argument
+ * Return 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}	
+	else 
+		return (0);
+}
