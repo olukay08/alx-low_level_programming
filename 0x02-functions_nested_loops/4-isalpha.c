35bf0ddcfbd36a0229e23652c1c252c050d12560
oluwole2044 <olukay08@gmail.com>
2022-09-13 20:42:44 -0700

    Utilizing function to solve tasks

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _isalpha - fuction to check if c is a letter,lowercase or uppercase
- * Return 0
+ * @c: is the int that will use for the argument in the function
+ *  Return: 1 for _isalpha and 0 for not aplaha
  */
 int _isalpha(int c)
 {
