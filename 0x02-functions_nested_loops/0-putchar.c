847a2b8abcde578a067303ed59624eae114744a6
oluwole2044 <olukay08@gmail.com>
2022-09-13 18:21:26 -0700

    Utilizing function to solve tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include "main.h"
-int main()
+/**
+ * main - Entry point
+ * Return: Always o (success)
+*/
+int main(void)
 {
 	_putchar('_');
-	_putchar('p');	
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
