55f3287ecb412bcbf73136cc2b5aa5e66f6dbb04
oluwole2044 <olukay08@gmail.com>
2022-09-14 03:46:21 -0700

    Utilizing function to solve tasks

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times other numbers starint from 0
+ * Return :Always 0;
+ */
+void times_table(void)
+{
+	int i, k, pr;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (k = 0 ; k <= 9 ; k++)
+		{
+			pr = i * k ;
+			_putchar(pr);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
