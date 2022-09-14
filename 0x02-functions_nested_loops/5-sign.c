26f012354b78c84c3ba8592368d39d0c93e250b4
oluwole2044 <olukay08@gmail.com>
2022-09-13 21:50:07 -0700

    Utilizing function to solve tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,16 @@
 /**
  * print_sign -function to check for the sign of a number
  * @n: is the int that wil use for the argument of the function
- * Return 1 for +sign -1 for -sign and 0 for zero
+ * Return:Always 1 for print_sign 1 print sign -1 for print_sign and 0 for zero
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	       	_putchar('+');
+		_putchar('+');
 		return (1);
 	}
-	 if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
