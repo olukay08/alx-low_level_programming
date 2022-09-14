e8db4894512a537b652decf89e99a50934a56b12
oluwole2044 <olukay08@gmail.com>
2022-09-13 18:42:43 -0700

    Utilizing function to solve tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print alphabets - function to print abac
+ *
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a' ;c <= 'z' ; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
