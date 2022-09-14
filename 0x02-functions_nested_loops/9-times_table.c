da09235f6b8cf432b47071065cc1d579cb4157d0
oluwole2044 <olukay08@gmail.com>
2022-09-14 04:10:42 -0700

    Utilizing function to solve tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,17 @@ void times_table(void)
 		for (k = 0 ; k <= 9 ; k++)
 		{
 			pr = i * k ;
+			if (pr < 10 )
+			{
 			_putchar(pr / 10 + '0');
 			_putchar(pr % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
+			}
+			else 
+				_putchar(pr % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
 		}
 		_putchar('\n');
 	}
