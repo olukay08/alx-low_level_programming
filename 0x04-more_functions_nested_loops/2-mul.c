b303966d24f4d630a8a911fcc5266da8d020052b
oluwole2044 <olukay08@gmail.com>
2022-09-15 14:53:14 -0700

    c function to solve complex task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - functions to mutiply two arguments which are integers
+ * @a: first int argument
+ * @b: second int argument
+ * Return:0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+	return (0);
+
+}
