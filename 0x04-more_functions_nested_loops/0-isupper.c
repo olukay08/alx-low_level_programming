8f0abadc4582f0505d37cbb0d4c61f51bddbd8e6
oluwole2044 <olukay08@gmail.com>
2022-09-15 14:09:57 -0700

    c function to solve complex task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 #include <ctype.h>
 /**
  * _isupper - function to check for uppercase in a character argument
- * @c: Argument for the function 
+ * @c: Argument for the function
  * Return: Always 1 for true and 0 for false
  */
 int  _isupper(int c)
 {
-	if ( isupper(c))
+	if (isupper(c))
 	{
 		return (1);
 	}
