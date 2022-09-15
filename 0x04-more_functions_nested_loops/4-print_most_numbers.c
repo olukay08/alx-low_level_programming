e2b2cabfb54a9006cba9bff15a96db77e19f210e
oluwole2044 <olukay08@gmail.com>
2022-09-15 15:57:10 -0700

    c function to solve complex task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers -print numbers frm 1 to 9 exludind 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int k;
+
+	for (k = 0; k < 10 ; k++)
+	{
+		if (k == 2 || k == 4)
+		{
+			continue;
+		}
+		_putchar(k + 'o');
+
+	}
+	_putchar('\n');
+
+}
