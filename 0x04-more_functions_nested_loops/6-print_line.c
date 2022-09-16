9939897869c51ed039342c73b256c5643d1dea92
oluwole2044 <olukay08@gmail.com>
2022-09-15 18:58:36 -0700

    c function to solve complex task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - tFunction draw a line
+ * @n:argument int of the function
+ * Return: 0
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+
+}
