c2b7c9e751270647c4cc8760a0ea0ed2e220469b
oluwole2044 <olukay08@gmail.com>
2022-09-16 05:14:00 -0700

    c function to solve complex task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main. -print numbers and substitute for the multiples of 5 and 3
+ * Return: 0
+ */
+int main(void)
+{
+	int g ;
+
+	for (g = 1; g <= 100 ; g++)
+	{
+		if((g % 3 == 0) && (g % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if(g % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if(g % 5 == 0);
+		{
+		
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ",g);
+		}
+	}
+	printf("\n");
+	return (0);
+}
