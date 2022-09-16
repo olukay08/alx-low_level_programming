f1d784af326ad17d0690a073b9eee2cf65237eee
oluwole2044 <olukay08@gmail.com>
2022-09-16 07:58:07 -0700

    c function to solve complex task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - print triangle structure
+ * @size:parameter
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int l, k;
+
+	if (l > 0)
+	{
+		for (l = 1; l <= size ; l++)
+		{
+			for (k = (size - l) ; k > 0 ; k--)
+			{
+				putchar(' ');
+			}
+			for (k = 0 ; k < l ; k++)
+			{
+				putchar('#');
+			}
+
+			if (l == size)
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+
+	}
+}
