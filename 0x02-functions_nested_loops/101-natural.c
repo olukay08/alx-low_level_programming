81dd9037ea1b19dbafbd837db0c6dc022f7632fe
oluwole2044 <olukay08@gmail.com>
2022-09-17 13:43:58 -0700

    Utilizing function to solve tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-/** 
+/**
  * main- print multiples of 3 and 5 below 1024
  * Return:0
  */
 int main(void)
 {
-	int sum;
+	int sum = 0;
 	int i;
 	int n = 1023;
 
 	for (i = 0; i <= n ; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-		{
+			{
 			sum += i;
-		}
+			}
+
+
 	}
 	printf("%d\n", sum);
 	return (0);
