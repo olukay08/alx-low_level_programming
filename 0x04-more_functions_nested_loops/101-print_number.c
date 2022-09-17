9ff88c0295ba0848d2be9b93ce418348a754339b
oluwole2044 <olukay08@gmail.com>
2022-09-16 19:17:29 -0700

    c function to solve complex task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,19 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	putchar('-');
+	_putchar('-');
 	num = -num;
 	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
 	if (n > 0)
 	{
 		print_number(num / 10);
 	}
 
-		putchar(num % 10 + '0');
+		_putchar(num % 10 + '0');
 
 }
