c056ac1e9726f0e80bb80f6fc2e68c98ad0003d4
oluwole2044 <olukay08@gmail.com>
2022-09-19 13:01:56 -0700

    C is fun with pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - To interchnage the values of two variables through pointers
+ * @a:fisrt parameter
+ * @b:Second parameter
+ * Return:Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	b* = 98;
+}
