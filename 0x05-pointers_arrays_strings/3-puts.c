9221a91ba9361dce6b5a5e4798c09a43f823071b
oluwole2044 <olukay08@gmail.com>
2022-09-19 14:51:16 -0700

    C is fun with pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts  - us for printing strings and new line to stdout
+ * @str:parameter
+ * Return: numbers of characters
+ */
+void _puts(char *str)
+{
+	return (puts(str));
+}
+
