019feb64a4fe65e19322a7ebab5439d7eee8306d
oluwole2044 <olukay08@gmail.com>
2022-09-20 12:29:17 -0700

    C is fun with pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - print half of the chrs of a string
+ * @string: prameter
+ * Return:nothing
+ */
+void puts_half(char *str)
+{
+	int g = strlen(str);
+	int i;
+	int h = g / 2;
+
+	for (i = 0 ; i < g; i++)
+	{
+		if (i >= h)
+		{
+			if (g % 2 == 0)
+			{
+
+			printf("%c", str[i]);
+			}
+			else
+			{
+				printf("%c", str[(g - 1)/2]);
+			}
+		}
+
+	}
+	printf("\n");
+
+}
