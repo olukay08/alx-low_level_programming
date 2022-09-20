473c9eac3048e973153e8e10c76fff29b2e722d3
oluwole2044 <olukay08@gmail.com>
2022-09-19 21:48:25 -0700

    C is fun with pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints other chars
+ * @str:parameter
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int l;
+
+	while (*(str + l) != '\0')
+	{
+		if (l % 2 == 0)
+		{
+			puctchar(*(str + l);
+		
+		}
+		i++;
+		
+	}
+	putchar(10);
+}
