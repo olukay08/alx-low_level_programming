1b93d12d1a424f9749604f7a6d7aea469f3fd939
oluwole2044 <olukay08@gmail.com>
2022-09-20 14:06:16 -0700

    C is fun with pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy- - Function to copy string from src to destination
+ * @dest: destination 
+ * @src:source to copy from
+ * Return:har
+ */
+char *_strcpy(char *dest, char *src)
+{
+	Return (strcpy(dest,src));
+}
+
