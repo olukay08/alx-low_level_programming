d2e6058188d9923294eeb3c533ac6fc03e5870c9
oluwole2044 <olukay08@gmail.com>
2022-09-21 13:45:18 -0700

    using arrrays and pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcat - function to concatenate two strings
+ * @dest:destination str
+ * @src:source parameter
+ * Return:chars
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
