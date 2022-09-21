eb073ade91c4b1fbb1f73596885b3a96aa73ec65
oluwole2044 <olukay08@gmail.com>
2022-09-21 14:09:30 -0700

    using arrrays and pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncat - it joins strings togethers
+ * @dest:parameter
+ * @src:second string parameter
+ * @n:no of bytes
+ * Return:chars
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
