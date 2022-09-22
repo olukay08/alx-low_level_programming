9c2f562166ee5c9c6fd0899e61c4d3f32ce50330
oluwole2044 <olukay08@gmail.com>
2022-09-22 09:47:16 -0700

    using arrrays and pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* *string_toupper - change string to upper
+*@g:parameter
+*Return:char
+*/
+char *string_toupper(char *g)
+{
+	int i = 0;
+
+	while (g[i])
+	{
+		putchar(toupper(g[i]));
+		i++;
+	}
+}
+
+
+
+
