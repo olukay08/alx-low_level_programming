ae7b6352df06acf164306500a66694866e15ca36
oluwole2044 <olukay08@gmail.com>
2022-09-26 14:32:23 -0700

    pointer and array

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,11 +5,11 @@
  * *_memset - function that fills memory with a constant byte
  * @s:parameter 1
  * @b:parameter 2
- * @n: interger bytes 
+ * @n: interger bytes
  * Return:chars
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s,b,n));
+	return (memset(s, b, n));
 }
-			
+
