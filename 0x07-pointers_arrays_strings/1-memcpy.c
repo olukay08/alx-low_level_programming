94598a8605cabc43bd4398d31d18271143a3ec67
oluwole2044 <olukay08@gmail.com>
2022-09-26 14:45:14 -0700

    pointer and array

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_memcpy - function that copies memory area
+ * @dest:pra 1
+ * @src:para 2
+ * @n: integer bytes
+ * Return:chars
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
