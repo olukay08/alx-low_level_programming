beb371d87cc08c8f8bfb6a7589933f93717f6d67
oluwole2044 <olukay08@gmail.com>
2022-09-26 15:14:22 -0700

    pointer and array

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include <srting.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s:para 1
+ * @accept:pra 2
+ * Return:chars
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strnspn(s, accept));
+	return (0);
+}
