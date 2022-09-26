f5ae9c8a01307013d28c3e0d348671b25120c70b
oluwole2044 <olukay08@gmail.com>
2022-09-26 15:03:03 -0700

    pointer and array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strchr - that locates a character in a string
+ * @s:para 1
+ * @c:para 2
+ * Return:chars
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
