19c8dc980e60d9ee67b9c7a6124da7877057e1c2
oluwole2044 <olukay08@gmail.com>
2022-09-27 08:01:45 -0700

    pointer and array

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  set_string - set the value of a pointer to char
+ *  @s:pra 1
+ *  @to:para 2
+ *  Return:void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
