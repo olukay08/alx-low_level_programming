a6e3260c7f839288256a9852cba164dbc74a426a
oluwole2044 <olukay08@gmail.com>
2022-09-30 04:47:02 -0700

    Using argc and argv in c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ * @argc:para 1
+ * @argv:para 2
+ * Return:0 for success
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
