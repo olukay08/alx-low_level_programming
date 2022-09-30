48b5555aedab7755d77777b3fdcfbd666bcd3b5f
oluwole2044 <olukay08@gmail.com>
2022-09-30 12:43:37 -0700

    Using argc and argv in c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,15 @@ int main(int argc, char *argv[])
 {
 	int r = 0;
 	int k, l;
-	if(argc > 1 && argc < 4)
-	{
-		k = atoi(argv[1]);
-		l = atoi(argv[2]);
-		r = k * l;
-		printf("%d\n", r);
-	}
+
 	if (argc != 3)
 	{
 		puts("Error");
 		return (1);
 	}
-	return (0);
+		k = atoi(argv[1]);
+		l = atoi(argv[2]);
+		r = k * l;
+		printf("%d\n", r);
+		return (0);
 }
