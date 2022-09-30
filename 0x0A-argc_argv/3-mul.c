035e35084b4081fbe15aa7b872784106a21f209e
oluwole2044 <olukay08@gmail.com>
2022-09-30 07:06:21 -0700

    Using argc and argv in c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc:para1
+ * @argv:pra2
+ * Return:0 and 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int r;
+	int k, l;
+
+	if (argc > 1)
+	{
+		k = atoi(argv[1]);
+		l = atoi(argv[2]);
+		r = k * l;
+		printf("%d\n", r);
+	}
+	if (argc == 1)
+	{
+		puts("Error");
+		return (1);
+	}
+	return (0);
+}
