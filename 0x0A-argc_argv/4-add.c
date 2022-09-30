a774a0b7fba8d2ff5bd09e86af72661b38f319c7
oluwole2044 <olukay08@gmail.com>
2022-09-30 15:31:09 -0700

    Using argc and argv in c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - print the sum of positive numbers
  * @argc :pra1
@@ -12,7 +13,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
+		if (atoi(argv[i]) >= 1)
 		{
 			num += atoi(argv[i]);
 		}
