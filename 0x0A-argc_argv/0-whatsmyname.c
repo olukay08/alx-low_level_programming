fcfcc109c594c7bbf157a53d3823329263d42b70
oluwole2044 <olukay08@gmail.com>
2022-09-30 04:32:24 -0700

    Using argc and argv in c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 /**
  * main - prints its name
  * @argc:counts the number of arguments in the program
- * @argv: array of strings 
+ * @argv: array of strings
  * Return: o for success
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
