fb63cfc0888d33465ca3dc7fe80410095d3dbad8
oluwole2044 <olukay08@gmail.com>
2022-10-10 04:31:16 -0700

    structure and typedef task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name:first member
+ * @age:sec member
+ * @owner:Third menmber
+ * Description:Same as above
+ */
+ struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
